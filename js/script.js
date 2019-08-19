42033611d7d97198b66a68ea9490a49158077597
Anise Lester, christine frank and joel stockamp <joel@>
2019-08-19 15:36:14 -0700

    Change to object to hold words instead of array

--- js/script.js
+++ js/script.js
@@ -6,22 +6,41 @@ $(document).ready(function() {
     var words = $("#input").val().split(" ");
 
     words.forEach(function(word, index){
-      var regex = /[,.;'"]/g
+      var regex = /[,.;'"()]/g
       words[index] = word.toLowerCase().replace(regex, "");
     });
     words.sort();
+    // >>>>>>  Uses Arrays  <<<<<<<<
+    // var wordCounter = [];
+    // words.forEach(function(word,index){
+    //   var firstIndex = words.indexOf(word);
+    //   if (word && words[index+1] !== word){
+    //     var count = (index - firstIndex) + 1;
+    //     wordCounter.push(word + " : " + count);
+    //   }
+    // });
+    // wordCounter.forEach(function(countedWord){
+      //   $("#result-list").append("<li>" + countedWord + "</li>");
+      // });
 
-    var wordCounter = [];
+    // >>>>>>  Uses Object  <<<<<<<<
+    var wordHolder;
     words.forEach(function(word,index){
       var firstIndex = words.indexOf(word);
       if (word && words[index+1] !== word){
+        console.log(word, typeof word);
         var count = (index - firstIndex) + 1;
-        wordCounter.push(word + " : " + count);
+        try{
+          wordHolder[word] = count;
+        } catch (e){
+          console.log("error...");
+        }
       }
     });
 
-    wordCounter.forEach(function(countedWord){
-      $("#result-list").append("<li>" + countedWord + "</li>");
+    Object.keys(words).forEach(function(countedWord){
+       $("#result-list").append("<li>" + countedWord + "</li>");
     });
+
   });
 });
