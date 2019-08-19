9146c8865b0d6c0cde37bd8faf3e5cd760ac7a6a
Anise Lester, christine frank and joel stockamp <anise@>
2019-08-19 15:16:39 -0700

    Add js logic to display sorted words with count of user input

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
 $(document).ready(function() {
+
   $("#words").submit(function(event){
     event.preventDefault();
-    var words = $("#input").val().split(" ");
-    words.sort();
-    var wordCounter = words.map(function(word,index){
-      var firstIndex = words.indexOf(word);
-      if(index === firstIndex && words[index+1] !=== word){
-        return word + ":1";
-      } else if (words[index+1] !=== word){
-        var count = (index - firstIndex) + 1;
 
+    var words = $("#input").val().split(" ");
+
+    words.forEach(function(word, index){
+      var regex = /[,.;'"]/g
+      words[index] = word.toLowerCase().replace(regex, "");
+    });
+    words.sort();
+
+    var wordCounter = [];
+    words.forEach(function(word,index){
+      var firstIndex = words.indexOf(word);
+      if (word && words[index+1] !== word){
+        var count = (index - firstIndex) + 1;
+        wordCounter.push(word + " : " + count);
       }
+    });
+
+    wordCounter.forEach(function(countedWord){
+      $("#result-list").append("<li>" + countedWord + "</li>");
     });
   });
 });
